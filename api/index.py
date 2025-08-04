e5d68ee525959854ece3aecb6a28553d09c0dfcd
CloudRain13 <s75177759@gmail.com>
2025-08-04 21:04:22 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,27 +1,9 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-
-@app.route("/api", methods=['POST'])
-def callback():
-    signature = request.headers.get('X-Line-Signature', '')
-    body = request.get_data(as_text=True)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text(event):
-    reply = TextSendMessage(text="你說的是：" + event.message.text)
-    line_bot_api.reply_message(event.reply_token, reply)
+@app.route("/api", methods=["POST"])
+def webhook():
+    print("✅ Webhook received!")
+    print(request.json)  # 觀察 LINE 傳來什麼
+    return "OK", 200
